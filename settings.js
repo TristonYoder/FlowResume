b815ef20645df533c82cc1275f02c58914e48706
Triston Yoder <triston@triston.me>
2016-10-25 10:48:31 -0400

    Created functions.js and settings.js
    Created functions.js
    - In progress: dynamicaly resize text to fit table
    Created settings.js
    - houses JSON resume
    - Contains all data to be entered into resume.
    - Commented examples of format in file

---
+++ settings.js
@@ -0,0 +1,102 @@
+var resume = 
+    {
+        "general" :
+        {
+            "firstName" : "Triston",
+            "lastName" : "Yoder",
+            "jobTitle" : "Software Developer",
+        },
+    
+        "contact" : {
+            //phone number with "." as seporator (ie: 123.456.7890)
+            "phone" : "123.456.7890",
+            "email" : "triston@triston.me",
+            "address" : null
+        },
+        
+        "summary" :
+            {
+            "title" : "Summary",
+            "body" : "Software Developer seeking internship in Computer Science or closely related field. Quickly masters new software packages and hardware technologies. Experience with Enterprise Resource Planning and Customer Relationship Management software."
+        },
+        
+        "timeline" : 
+            {
+            "title" : "Timeline",
+            "jobs" :
+                [ //["Date", "Title", "Description"],
+                    ["1997", "Birth", "My memory is a bit fuzzy on this one"],
+                    ["2011", "High School", "Tested the ability to fail P.E."],
+                    ["2015", "First Job as a Programmer", "Worked at Solution Source"],
+                    ["2015", "University", "Started college at Indiana-Purdue Fort-Wayne"],
+                    ["2016", "University: Part 2", "Transfered to Purdue Main-Campus"],
+                    ["2017", "I want to join your team", null]
+                ]
+        },
+    
+        "workHistory" : 
+            [
+  /*            ["Company", "Title", "Address", "Dates", [
+                    "bullet 1",
+                    "bullet 2",
+                    "bullet 3"
+                    ]
+                ],                                              */
+
+                ["Solution Source", "Software Development Intern", "1769 Stafford Court Goshen, IN 46526", "07/2015 to 08/2016", [
+
+                    "Designed, developed, and implemented automated testing platform for custom applications on NetSuite platform",
+
+                    "Created demonstration module to extend testing platform, allowing potential clients to experience the system with data similar to their own.",
+
+                    "Wrote and implemented scripts to enhance user experience and integrated scripts with entire system",
+
+                    "Trained and acted as reference for new employees working with NetSuite API",
+
+                    "Identified and suggested new technologies and tools for enhancing product value and increasing team productivity",
+
+                    "Assisted coworkers with company transition from outdated version control system to Git-based system",
+
+                    "Worked with many API's on various projects simultaneously",
+
+                    "Modified existing software to correct errors, adapt to new features and deprecation thereof as well as improve performance"
+
+                    ]
+                ],
+
+                ["Menards Inc.", "Front End Associate", "1925 Lincolnway East, Goshen, IN 46526", "07/2015 to 08/2016", [
+
+                    "Understood customer issues and found or created solutions to resolve them",
+
+                    "Managed 10-20 Front End employees, orchestrating efficient and enjoyable experience for customers and those managed",
+
+                    "Increased department efficiency by ensuring employees were working in the capacity they were most effective."
+
+                    ]
+                ]
+            ],
+
+        "skills" :
+            {
+            "title" : "Skills",
+            "points" : [
+                "E-Commerce Experience",
+                "ERP/CRM Development",
+                "Object Oriented Development",
+                "Script Development",
+                "Web Development",
+                "Works well in teams",
+                "Advanced problem solving skills",
+                "Innovative"
+            ]
+        },
+    
+        "education" :
+            {
+            "title" : "Education",
+            "points" : [
+//              ["University", "Degree", "Major", "Year", "Location"],
+                ["Purdue University", "Bachelor of Science", "Computer Science", "2019", "West Lafayette, Indiana, USA"]
+            ]
+        }
+    }
